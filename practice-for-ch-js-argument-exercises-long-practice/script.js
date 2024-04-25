6080aa4145a3fb3eee9822303cb787dd459f5886
Rafa-Camp04 <rafaelcampos04@hotmail.com>
2024-04-25 17:08:06 -0400

    mybind done

--- practice-for-ch-js-argument-exercises-long-practice/script.js
+++ practice-for-ch-js-argument-exercises-long-practice/script.js
@@ -1,12 +1,12 @@
-function sum() {
-    let res = 0
+// function sum() {
+//     let res = 0
 
-    for (let i = 0; i < arguments.length; i++) {
-        res += arguments[i]
-    }
+//     for (let i = 0; i < arguments.length; i++) {
+//         res += arguments[i]
+//     }
 
-    return res 
-}
+//     return res 
+// }
 
 function sumRest(...args) {
     let res = 0
@@ -68,27 +68,49 @@ class Cat {
 const markov = new Cat("Markov");
 const pavlov = new Dog("Pavlov");
 
-markov.says("meow", "Ned");
+// markov.says("meow", "Ned");
 // Markov says meow to Ned!
 // true
 
 // bind time args are "meow" and "Kush", no call time args
-markov.says.myBind(pavlov, "meow", "Kush")();
+// markov.says.myBind(pavlov, "meow", "Kush")();
 // Pavlov says meow to Kush!
 // true
 
 // no bind time args (other than context), call time args are "meow" and "a tree"
-markov.says.myBind(pavlov)("meow", "a tree");
+// markov.says.myBind(pavlov)("meow", "a tree");
 // Pavlov says meow to a tree!
 // true
 
 // bind time arg is "meow", call time arg is "Markov"
-markov.says.myBind(pavlov, "meow")("Markov");
+// markov.says.myBind(pavlov, "meow")("Markov");
 // Pavlov says meow to Markov!
 // true
 
 // no bind time args (other than context), call time args are "meow" and "me"
-const notMarkovSays = markov.says.myBind(pavlov);
-notMarkovSays("meow", "me");
+// const notMarkovSays = markov.says.myBind(pavlov);
+// notMarkovSays("meow", "me");
 // Pavlov says meow to me!
 // true  
+
+
+
+function curriedSum(num) {
+
+  let sum = 0
+  let count = 0
+
+    return function _add(num2) {
+      sum += num2
+      count += 1
+      if (count === num) { 
+        return sum
+      } else {
+        return _add
+      }
+    }
+}
+
+
+const sum = curriedSum(4);
+console.log(sum(5)(30)(20)(1)); // => 56
